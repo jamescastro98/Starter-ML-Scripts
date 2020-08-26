d3ca2e9ee282ba68597da154483b32ac6789fb09
jamescastro98 <jamescastro9898@gmail.com>
2020-08-26 01:50:30 -0300

    Started basic functionality for security cam

--- vision.py
+++ vision.py
@@ -1,11 +1,29 @@
 import numpy as np
 import cv2
 import sys
+import time
 from PIL import *
+import json
+from datetime import datetime
+
+detected=True
+curr=(datetime.now()).seconds
+
+def alert(date,img):
+    global detected
+    if(detected==True):
+        detected=False
+        #date=datetime.now()
+        exactTime=date.strftime("%X")
+        day=date.strftime("%a")
+        month=date.strftime("%b")
+        year=date.strftime("%Y")
+        print(exactTime+" " + day +" " + month + " " +year)
+
 f=cv2.face.LBPHFaceRecognizer_create()
 faceCascade = cv2.CascadeClassifier('visiondata/haarcascade_frontalface_default.xml')
-eyeCascade=cv2.CascadeClassifier('visiondata/haarcascade_eye.xml')
-
+eyeCascade=cv2.CascadeClassifier('visiondata/haarcascade_eye_tree_eyeglasses.xml')
+timecaught=0
 vid=cv2.VideoCapture(0)
 while(vid.isOpened()==True):
     ret,frame = vid.read()
@@ -13,6 +31,15 @@ while(vid.isOpened()==True):
         gray=cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
         faces=faceCascade.detectMultiScale(gray,1.3,5)
         for(x,y,w,h) in faces:
+            curr=datetime.now()
+            alert(curr,"hello")
+            #edge case with midnight need to do
+            '''if(curr-timecaught>3) and (curr-timecaught<300):
+                #Alert Person at door and stop counting time for a little
+                alert("hello")
+            timecaught=curr
+            '''
+            
             cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),2)
             roi_gray=gray[y:y+h,x:x+w]
             roi_color=frame[y:y+h,x:x+w]
@@ -20,11 +47,10 @@ while(vid.isOpened()==True):
             for(ex,ey,ew,eh) in eyes:
                 cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
         cv2.imshow('Frame',frame)
-        if cv2.waitKey(25) & 0xFF == ord('q'):
-            cv2.destroyAllWindows()
-            vid.release()
+        if cv2.waitKey(15) & 0xFF == ord('q'):
             break
     else:
         print("No Image")
-#cv2.destroyAllWindows()
-#vid.release()
+vid.release()
+cv2.destroyAllWindows()
+sys.exit(0)
